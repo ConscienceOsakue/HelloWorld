4bcccc64aefb486b5b52eb329feb238a6e1892d8
conscience osakue <consciencenosa@gmail.com>
2023-11-09 20:57:56 -0600

    Updating the modified Program.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("It's Thursday evening. Wish it was Sunday.");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It's Thursday evening. Wish it was Sunday.");
+        }
+    }
+}
